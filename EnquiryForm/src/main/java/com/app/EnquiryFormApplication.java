6921643cf5a085e38c89fc604ce68160579880ef
Navnath3864 <nagutte0@gmail.com>
2025-01-22 14:04:39 +0530

    All problem solved by Navnath

--- EnquiryForm/src/main/java/com/app/EnquiryFormApplication.java
+++ EnquiryForm/src/main/java/com/app/EnquiryFormApplication.java
@@ -2,10 +2,19 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EnquiryFormApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EnquiryFormApplication.class, args);
+	}
+	@Bean
+	//@LoadBalanced
+	public RestTemplate rt() {
+		return new RestTemplate();
 	}
 }
