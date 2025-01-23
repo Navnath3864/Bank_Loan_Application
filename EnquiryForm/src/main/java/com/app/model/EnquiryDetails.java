28a0b2d089daf2a5f7271345b075bbdd8864a599
Navnath3864 <nagutte0@gmail.com>
2025-01-23 23:03:16 +0530

    delete this branch

--- EnquiryForm/src/main/java/com/app/model/EnquiryDetails.java
+++ EnquiryForm/src/main/java/com/app/model/EnquiryDetails.java
@@ -2,12 +2,16 @@ package com.app.model;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Entity
@@ -16,12 +20,22 @@ public class EnquiryDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int customerID;
+	
+	@NotBlank(message = "First Name is required")
 	private String firstName;
+	
+	@NotBlank(message = "Last Name is required")
 	private String lastName;
+	
 	private int age;
+	
+	@Email(message = "Invalid Email Id")
 	private String email;
+	
+//	@Pattern(regexp = "^[6-9]\\d{9}$", message = "Invalid mobile number")
 	private long mobileNo;
-
+	
+//	@NotBlank(message = "PancardNo is required")
 	private String pancardNo;
 	
 	@OneToOne(cascade = CascadeType.ALL)
