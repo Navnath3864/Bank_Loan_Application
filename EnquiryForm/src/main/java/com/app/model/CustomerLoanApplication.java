1cd3f9492380631d4e45240fda3b057d30ac5b3e
juhee2001 <juheeupadhye13@gmail.com>
2025-01-24 16:15:21 +0530

    CustomerLoanApplication class added in com.app.model package by juhee

---
+++ EnquiryForm/src/main/java/com/app/model/CustomerLoanApplication.java
@@ -0,0 +1,48 @@
+package com.app.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class CustomerLoanApplication {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int customerId;
+	private String customerName;
+	private String customerDateOfBirth;
+	private int customerAge;
+	private int requiredTenure;
+	private String customerGender;
+	private String customerEmail;
+	private double customerMobileNumber;
+	private double customerAdditionalMobileNumber;
+	private double customerAmountPaidForHome;
+	private double customerTotalLoanRequired;
+	private String loanStatus;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private AllPersonalDocs allpersonalDocument;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private DependentInfo familydependentInfo;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	private CustomerAddress customerAddress;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private CibilScoreData cibilScoreData;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	private AccountDetails accountdetails;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	private GuarantorDetails gurantordetails;
+	
+}
