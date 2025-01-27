82b0292545c5857610de460839938c2bf56a0a7b
juhee2001 <juheeupadhye13@gmail.com>
2025-01-27 15:51:46 +0530

    changes

--- EnquiryForm/src/main/java/com/app/service/CustomerLoanApplicationService.java
+++ EnquiryForm/src/main/java/com/app/service/CustomerLoanApplicationService.java
@@ -1,9 +1,19 @@
 package com.app.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.app.model.CustomerLoanApplication;
+
+import jakarta.validation.Valid;
 
 public interface CustomerLoanApplicationService {
 
-	public CustomerLoanApplication saveDetails(CustomerLoanApplication customerLoanApplication, int id);
+	//public CustomerLoanApplication saveDetails(CustomerLoanApplication customerLoanApplication, int id);
 
+	
+	public CustomerLoanApplication saveDetails(String customerLoanApplication, int id, MultipartFile addressProof,
+			MultipartFile panCard, MultipartFile incomeTax, MultipartFile addharCard, MultipartFile photo,
+			MultipartFile signature, MultipartFile bankCheque, MultipartFile salarySlips);
+
+	
 }
