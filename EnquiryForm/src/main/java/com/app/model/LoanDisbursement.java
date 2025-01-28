2c062a2f16b348392adf105b037068a14709dee6
DELL-PC <DELL-PC@PC>
2025-01-28 17:22:21 +0530

    add modelclasses

--- EnquiryForm/src/main/java/com/app/model/LoanDisbursement.java
+++ EnquiryForm/src/main/java/com/app/model/LoanDisbursement.java
@@ -1,5 +1,26 @@
 package com.app.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class LoanDisbursement {
+@Id
+
+private int agreementId;
+private int loanNo;
+private String agreementDate;
+private String amountPayType;
+private Double totalAmount;
+private String bankName;
+private Long accountNumber;
+private String IFSCCode;
+private String accountType;
+private Double transferAmount;
+private String paymentStatus;
+private String amountPaidDate;
+
 
 }
