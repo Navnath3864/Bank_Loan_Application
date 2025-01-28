16b272204c426380c4a2d5c4e34e51d8d9dd87e3
juhee2001 <juheeupadhye13@gmail.com>
2025-01-28 16:57:44 +0530

    add another classes

--- EnquiryForm/src/main/java/com/app/model/CustomerLoanApplication.java
+++ EnquiryForm/src/main/java/com/app/model/CustomerLoanApplication.java
@@ -15,6 +15,7 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.websocket.OnError;
 import lombok.Data;
 
 @Entity
@@ -89,4 +90,15 @@ public class CustomerLoanApplication {
 	@Valid
 	private GuarantorDetails gurantorDetails;
 	
+	@OneToOne
+	private LoanDisbursement loanDisbursement;
+	
+	@OneToOne
+	private Ledger ledger;
+	
+	@OneToOne
+	private SanctionLetter sanctionLetter;
+	
+	@OneToOne
+	private CustomerVerification customerVerification;
 }
