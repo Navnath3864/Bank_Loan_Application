76e30e7c38410e100fa1e525c9bc7aadda5289fd
aman <77562702+amankhadse461@users.noreply.github.com>
2025-02-03 22:03:39 +0530

    add save ledger api by aman

--- EnquiryForm/src/main/java/com/app/service/LedgerService.java
+++ EnquiryForm/src/main/java/com/app/service/LedgerService.java
@@ -1,9 +1,11 @@
 package com.app.service;
 
+import java.util.List;
+
 import com.app.model.Ledger;
 
 public interface LedgerService {
 
-	Ledger saveLedgerData(Ledger ledger,int customerLoanID);
+	List<Ledger> saveLedgerData(Ledger ledger,int customerLoanID);
 	
 }
--- EnquiryForm/src/main/java/com/app/serviceimpl/LedgerServiceImpl.java
+++ EnquiryForm/src/main/java/com/app/serviceimpl/LedgerServiceImpl.java
@@ -1,5 +1,8 @@
 package com.app.serviceimpl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,19 +19,27 @@ public class LedgerServiceImpl implements LedgerService {
 
 	@Autowired
 	CustomerLoanApplicationServiceImpl customerLoanApplicationServiceImpl;
-
+	
+	
 	@Override
-	public Ledger saveLedgerData(Ledger ledger, int customerLoanID) {
-		CustomerLoanApplication custApplication = customerLoanApplicationServiceImpl
-				.getCustomerLoanApplication(customerLoanID);
-		ledger.setLedgerId(custApplication.getCustomerLoanID());
-		ledger.setTotalLoanAmount(custApplication.getLoandisbursement().getTotalAmount());
-		ledger.setTenure(custApplication.getRequiredTenure());
-		ledger.setMonthlyEMI(ledger.getTotalLoanAmount() / ledger.getTenure());
-		ledger.setRemainingAmount(ledger.getTotalLoanAmount() - ledger.getAmountPaidtillDate());
+	public List<Ledger> saveLedgerData(Ledger ledger1, int customerLoanID) {
+	    CustomerLoanApplication custApplication = customerLoanApplicationServiceImpl
+	            .getCustomerLoanApplication(customerLoanID);
+	    int count = 0;
+	    List<Ledger> ledgers = custApplication.getLedger();
 
-		return ledgerRepository.save(ledger);
+	    while ( count < custApplication.getRequiredTenure() ) {
+	        Ledger ledger = new Ledger();
+	        ledger.setTotalLoanAmount(custApplication.getLoandisbursement().getTotalAmount());
+	        ledger.setTenure(custApplication.getRequiredTenure());
+	        ledger.setMonthlyEMI(ledger.getTotalLoanAmount() / ledger.getTenure());
+	        ledgers.add(ledger);
+	        count++;
+	    }
 
+	    ledgerRepository.saveAll(ledgers); 
+	    custApplication.getLedger().addAll(ledgers); 
+	    return custApplication.getLedger();
 	}
 
 }
