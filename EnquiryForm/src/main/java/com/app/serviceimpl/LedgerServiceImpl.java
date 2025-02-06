61570cfe6b81aa63268e8dd4765ae61261611357
aman <77562702+amankhadse461@users.noreply.github.com>
2025-02-06 21:04:49 +0530

    changes made by aman in updateledeger api

--- EnquiryForm/src/main/java/com/app/serviceimpl/LedgerServiceImpl.java
+++ EnquiryForm/src/main/java/com/app/serviceimpl/LedgerServiceImpl.java
@@ -1,5 +1,4 @@
 package com.app.serviceimpl;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -19,64 +18,92 @@ public class LedgerServiceImpl implements LedgerService {
 
 	@Autowired
 	CustomerLoanApplicationServiceImpl customerLoanApplicationServiceImpl;
-	
-	
+
 	@Override
 	public List<Ledger> saveLedgerData(Ledger ledger1, int customerLoanID) {
-	    CustomerLoanApplication custApplication = customerLoanApplicationServiceImpl
-	            .getCustomerLoanApplication(customerLoanID);
-	    int count = 0;
-	    List<Ledger> ledgers = custApplication.getLedger();
+		CustomerLoanApplication custApplication = customerLoanApplicationServiceImpl
+				.getCustomerLoanApplication(customerLoanID);
+		int count = 0;
+		List<Ledger> ledgers = custApplication.getLedger();
 
-	    while ( count < custApplication.getRequiredTenure() ) {
-	        Ledger ledger = new Ledger();
-	        ledger.setTotalLoanAmount(custApplication.getLoandisbursement().getTotalAmount());
-	        ledger.setTenure(custApplication.getRequiredTenure());
-	        ledger.setMonthlyEMI(ledger.getTotalLoanAmount() / ledger.getTenure());
-	        ledgers.add(ledger);
-	        count++;
-	    }
+		while (count < custApplication.getRequiredTenure()) {
+			Ledger ledger = new Ledger();
+			ledger.setTotalLoanAmount(custApplication.getLoandisbursement().getTotalAmount());
+			ledger.setTenure(custApplication.getRequiredTenure());
+			ledger.setMonthlyEMI(ledger.getTotalLoanAmount() / ledger.getTenure());
+			ledger.setLoanStatus("pending");
+			ledgers.add(ledger);
+			count++;
+		}
 
-	    ledgerRepository.saveAll(ledgers); 
-	    custApplication.getLedger().addAll(ledgers); 
-	    return custApplication.getLedger();
+		ledgerRepository.saveAll(ledgers);
+		custApplication.getLedger().addAll(ledgers);
+		return custApplication.getLedger();
 	}
-
 
 	@Override
 	public Ledger updateledger(int id, String option) {
-		
-		if("pay".equals(option))
-		{
-			Optional<Ledger> ledger = ledgerRepository.findById(id);
-			Ledger ledger2 =ledger.get();
-			double p = 0;
-			double  h = ledger2.getMonthlyEMI();
-			
-			ledger2.setRemainingAmount(ledger2.getRemainingAmount()-h);
-			System.out.println(ledger2.getMonthlyEMI());
-			System.out.println(ledger2.getTotalLoanAmount());
-			ledger2.setMonthlyEMI(p);
-			ledgerRepository.save(ledger2);
-			return ledger2;
-			
-		}
-		if("skip".equals(option))
-		{
-			Optional<Ledger> ledger = ledgerRepository.findById(id);
-			Ledger ledger2 =ledger.get();
-			
-			Optional<Ledger> ledger1 = ledgerRepository.findById(id+1);
-			Ledger ledger3 =ledger1.get();
-			double  h = ledger2.getMonthlyEMI();
-			ledger3.setMonthlyEMI(h+ledger3.getMonthlyEMI());
-			ledgerRepository.save(ledger3);
-			return ledger2;
-			
+
+		Optional<Ledger> ledger = ledgerRepository.findById(id);
+		Optional<Ledger> led = ledgerRepository.findById(id - 1);
+		if (led.isEmpty()) {
+			Ledger ledger2 = ledger.get();
+			if ("pay".equals(option)) {
+				double emi = ledger2.getMonthlyEMI();
+				ledger2.setRemainingAmount(ledger2.getTotalLoanAmount() - emi);
+				ledger2.setCurrentMonthEmiStatus("paid");
+				ledger2.setAmountPaidtillDate(emi);
+				ledger2.setPreviousEmitStatus("unpaid");
+				ledger2.setMonthlyEMI(0.0);
+				if (ledger2.getRemainingAmount() ==0) {
+					ledger2.setLoanStatus("nill");
+				}
+				ledgerRepository.save(ledger2);
+				return ledger2;
+			}
+
+			if ("skip".equals(option)) {
+				Optional<Ledger> ledger1 = ledgerRepository.findById(id + 1);
+				Ledger ledger3 = ledger1.get();
+				double emi = ledger2.getMonthlyEMI();
+				ledger3.setMonthlyEMI(emi + ledger3.getMonthlyEMI());
+				ledger2.setCurrentMonthEmiStatus("unpaid");
+				ledgerRepository.save(ledger3);
+				return ledger2;
+
+			}
+		} else {
+			if (led != null) {
+				Ledger leg = led.get();
+				Ledger ledger2 = ledger.get();
+				if ("pay".equals(option)) {
+					double emi = ledger2.getMonthlyEMI();
+					ledger2.setRemainingAmount(leg.getRemainingAmount() - emi);
+					ledger2.setCurrentMonthEmiStatus("paid");
+					ledger2.setAmountPaidtillDate(leg.getAmountPaidtillDate() + emi);
+					ledger2.setPreviousEmitStatus(leg.getCurrentMonthEmiStatus());
+					ledger2.setMonthlyEMI(0.0);
+					if (ledger2.getRemainingAmount() ==0) {
+						ledger2.setLoanStatus("nill");
+					}
+					ledgerRepository.save(ledger2);
+					return ledger2;
+				}
+
+				if ("skip".equals(option)) {
+					Optional<Ledger> ledger1 = ledgerRepository.findById(id + 1);
+					Ledger ledger3 = ledger1.get();
+					double emi = ledger2.getMonthlyEMI();
+					ledger3.setMonthlyEMI(emi + ledger3.getMonthlyEMI());
+					ledger2.setCurrentMonthEmiStatus("unpaid");
+					ledgerRepository.save(ledger3);
+					return ledger2;
+
+				}
+			}
+
 		}
 		return null;
-		
-		
-	}
 
+	}
 }
