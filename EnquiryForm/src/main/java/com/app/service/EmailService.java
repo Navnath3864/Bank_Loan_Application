9a55d49910e7fa6eb1c517dd2f9d601aead01118
aman <77562702+amankhadse461@users.noreply.github.com>
2025-02-08 16:54:57 +0530

    add method to sent mail to customer with ledger deatils by aman

--- EnquiryForm/src/main/java/com/app/service/EmailService.java
+++ EnquiryForm/src/main/java/com/app/service/EmailService.java
@@ -1,9 +1,12 @@
 package com.app.service;
 
 import com.app.model.CustomerLoanApplication;
+import com.app.model.Ledger;
 
 public interface EmailService {
 
 	CustomerLoanApplication sendSanctionLetterMailToCustomer(int customer_ID);
+	
+	void sentLegderStatusToCustomerMail(int customerloadId,Ledger ledger );
 
 }
